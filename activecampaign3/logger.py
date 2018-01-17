834c4c24959664467a6030379655f5f7ed17fd37
Ana Nelson <ana@cosmify.io>
2018-01-17 11:43:11 -0800

    Make logging configurable.

--- activecampaign3/logger.py
+++ activecampaign3/logger.py
@@ -1,14 +1,16 @@
+from activecampaign3.config import CONFIG
 import logging
 import os
 
-logsdir = "logs"
+logsdir = CONFIG['logs']['dir']
+log_level = getattr(logging, CONFIG['logs']['level'].upper())
 
 logger = logging.getLogger("activecampaign3")
 os.makedirs(logsdir, exist_ok=True)
-logfilepath = os.path.join(logsdir, "debug.log")
+logfilepath = os.path.join(logsdir, CONFIG['logs']['file'])
 h = logging.FileHandler(logfilepath)
-h.setLevel(logging.DEBUG)
+h.setLevel(log_level)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 h.setFormatter(formatter)
 logger.addHandler(h)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(log_level)
