9ea2d8d302b3ea746fb47cfa10e449c2172d8dd8
Ana Nelson <ana@cosmify.io>
2018-01-17 11:42:58 -0800

    Add username to valid search params.

--- activecampaign3/user.py
+++ activecampaign3/user.py
@@ -2,7 +2,7 @@ from activecampaign3.resource import Resource
 
 class User(Resource):
     _resource_path = 'users'
-    _valid_search_params = []
+    _valid_search_params = ['username']
     _valid_save_params = "username email firstName lastName group password".split()
 
     # TODO shortcuts to look up by username, email, "me" - currently logged in user
